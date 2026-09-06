45fb696f1982628903eaebfd4448d19eeeb839b5
kakardousali-hash <k.a.kardous.ali@gmail.com>
2026-09-06 23:37:42 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
+const express = require("express");
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Kardous Survival is online! 🎮");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Kardous Survival running on port ${PORT}`);
+});
